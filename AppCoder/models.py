c10be616dc584a05ed72eb2eeffce02218313745
Cristian Pereyra <cristianniky@gmail.com>
2023-03-10 19:26:55 -0300

    Creando plantillas heredadas

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,6 +4,10 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField(unique=True)
 
+    def __str__(self):
+        return f"Curso: {self.nombre} {self.camada}"
+
+
 class Estudiantes(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
